f175cb7c1b44256689b7565db3985839bad01ac3
yarymaks <yarygin45@gmail.com>
2022-01-25 18:33:35 +0100

    update

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import { createUseStyles } from 'react-jss';
-
-const useStyles = createUseStyles({
-  Container: {
-    width: '300px',
-    textAlign: 'center',
-  },
-});
-
-const Container = ({ children }) => {
-  const classes = useStyles();
-  return <div className={classes.Container}>{children}</div>;
-};
-
-export default Container;
+export { default } from './Container';
